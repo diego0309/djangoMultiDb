909bd60cf1489f0501b30959547ad0543efe4baa
diego0309 <diegoosvaldo0309@gmail.com>
2018-11-29 23:16:14 -0600

    Inserción a través de form

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index,  name='index'),
     path('nuevo', views.nuevo,  name='nuevo'),
-    re_path(r'details/(?P<id>\d+)', views.details,  name='details'),
+    re_path(r'details/mysql/(?P<id>\d+)',
+            views.detailsMySql,  name='detailsMySql'),
+    re_path(r'details/sqlite/(?P<id>\d+)',
+            views.detailsSqLite,  name='detailsSqLite'),
+    re_path(r'details/mongo/(?P<id>\d+)',
+            views.detailsMongo,  name='detailsMongo'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Posts
-
+from .forms import PostForm
+from datetime import datetime
 # Create your views here.
 
 
 def index(request):
 
-    postsMysql = Posts.objects.using('ejemplo').all()[:10]
-    postsMongo = Posts.objects.using('mongo').all()[:10]
-    postsSqlite = Posts.objects.all()[:10]
+    postsMysql = Posts.objects.using('ejemplo').all()
+    postsMongo = Posts.objects.using('mongo').all()
+    postsSqlite = Posts.objects.all()
 
     context = {
         'title': 'Posts obtenidos desde mysql',
@@ -23,7 +24,17 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
-def details(request, id):
+def detailsSqLite(request, id):
+    post = Posts.objects.get(id=id)
+
+    context = {
+        'post': post
+    }
+
+    return render(request, 'posts/details.html', context)
+
+
+def detailsMySql(request, id):
     post = Posts.objects.using('ejemplo').get(id=id)
 
     context = {
@@ -33,8 +44,33 @@ def details(request, id):
     return render(request, 'posts/details.html', context)
 
 
-def nuevo(request):
+def detailsMongo(request, id):
+    post = Posts.objects.using('mongo').get(id=id)
+
     context = {
-        'title': 'Nuevo post'
+        'post': post
+    }
+
+    return render(request, 'posts/details.html', context)
+
+
+def nuevo(request):
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.created_At = datetime.now()
+            post.save(using='mongo')
+            context = {
+                'post': post
+            }
+            return redirect(detailsMongo, id=post.id)
+    else:
+        form = PostForm()
+
+    context = {
+        'title': 'Nuevo post',
+        'form': form
     }
     return render(request, 'posts/nuevo.html', context)
